7548edfd7767a8a45e36a75ebca5736ae0d769d6
Noodle-prog <valentina.breithuber@gmail.com>
2024-10-22 15:35:38 +0200

    ci-02: test

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -1,7 +1,14 @@
-package main.java.at.fhj.msd;
+package at.fhj.msd;
 
+/**
+ * Hello world!
+ *
+ */
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
+    }
+    public int minus(int a, int b) {
+        return a - b;
     }
 }
--- src/test/java/at/fhj/msd/CalculatorTest.java
+++ src/test/java/at/fhj/msd/CalculatorTest.java
@@ -1,13 +1,39 @@
-package test.java.at.fhj.msd;
-
-import org.junit.jupiter.api.Test;
+package at.fhj.msd;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class CalculatorTest {
 
-    @Test
-    public void testAdd() {
+    public static Stream<Object[]> provideAdditionData() {
+        return Stream.of(new Object[][]{
+            {1, 1, 0},
+            {5, 3, 2},
+            {4, 1, 3},
+            {0, 0, 0}
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "1, 1, 2",
+        "2, 3, 5",
+        "3, 3, 6",
+        "100, 200, 300"
+    })
+    public void testAdd(int a, int b, int expectedResult) {
         Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
+        assertEquals(expectedResult, calculator.add(a, b));
+    }
+    
+    @ParameterizedTest
+    @MethodSource("provideAdditionData")
+    public void testMinus(int a, int b, int expectedResult) {
+        Calculator calculator = new Calculator();
+        assertEquals(expectedResult, calculator.minus(a, b));
     }
 }
